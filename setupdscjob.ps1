972a32eaee1778c606e3754b009f3ef99bc0d2a2
tim-beer <tim@fignon.co.uk>
2016-09-29 15:47:56 +0100

    Add files via upload

---
+++ setupdscjob.ps1
@@ -0,0 +1,10 @@
+﻿$DCSaccount = Get-AzureRmAutomationAccount -Name 'Your Account Name' -ResourceGroupName 'Your Resource Group'
+
+$DCSaccount | Import-AzureRmAutomationDscConfiguration -SourcePath C:\setupdscjob.ps1 -Published -Force
+$DCSaccount | Get-AzureRmAutomationDscConfiguration -Name myfile1
+
+$DSCJob = $DCSaccount | Get-AzureRmAutomationDscConfiguration -Name myfile1 | Start-AzureRmAutomationDscCompilationJob
+
+While (-not($DSCJob | Get-AzureRmAutomationDscCompilationJob).EndTime){Start-Sleep -Seconds 5}
+
+$DCSaccount | Get-AzureRmAutomationDscNodeConfiguration
