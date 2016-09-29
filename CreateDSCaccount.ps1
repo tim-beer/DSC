6eedc7d40e1418515079b50117fc28b061e7c8ae
tim-beer <tim@fignon.co.uk>
2016-09-29 10:18:57 +0100

    Add files via upload

---
+++ CreateDSCaccount.ps1
@@ -0,0 +1,7 @@
+﻿#Create AzureAutomation Account
+Add-AzureRmAccount -SubscriptionName 'Enter Subscription Name'
+$ResourceGroup = New-AzureRmResourceGroup -Name 'Your New Resource Group Name' -Location 'West Europe'
+$ResourceGroup | New-AzureRmAutomationAccount -Name 'Your New Account Name' -Plan Free
+
+$Account = $ResourceGroup | Get-AzureRmAutomationAccount -Name 'Your New Account Name'
+$Account | Get-AzureRmAutomationRegistrationInfo
